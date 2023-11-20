5412d983397030c96d22a0422ada400341863aa3
gnehs <jayuiop@gmail.com>
2023-11-20 19:02:15 +0800

    updated layout.tsx

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,32 @@
 import { Outlet } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 export default function Layout() {
+  // redirect by language
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // get browser language
+    let lang = navigator.language;
+    if (location.pathname === "/") {
+      if (["zh-TW", "zh-CN"].includes(lang)) {
+        navigate("/zh-TW");
+      } else {
+        document.documentElement.lang = "en";
+        navigate("/en-US");
+      }
+    }
+  }, [location]);
   return (
     <>
       <Nav />
-      <div className="prose">
-        <Outlet />
+      <div className="container">
+        <div className="prose">
+          <Outlet />
+        </div>
       </div>
       <Footer />
     </>
