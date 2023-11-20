63e880545fba6ffbf68456f30863523c174ddf96
gnehs <jayuiop@gmail.com>
2023-11-20 17:29:02 +0800

    added prerender

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
+import seoPrerender from "vite-plugin-seo-prerender";
+import { globSync } from "glob";
 import react from "@vitejs/plugin-react-swc";
 import mdx from "@mdx-js/rollup";
+
+function parsePath(path: string) {
+  // "./content/**/*.mdx" -> "/**/*"
+  // "./content/**/index.mdx" -> "/**/"
+  return path
+    .replace("src/content", "")
+    .replace(/\/index.mdx$/, "/")
+    .replace(/\.mdx$/, "");
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [mdx({ providerImportSource: "@mdx-js/react" }), react()],
+  plugins: [
+    mdx({ providerImportSource: "@mdx-js/react" }),
+    react(),
+    seoPrerender({
+      routes: globSync("./src/content/**/*.mdx").map(parsePath),
+    }),
+  ],
 });
