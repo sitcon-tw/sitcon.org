c1ac2d0a28dcbef076d7513ba47fbdf1e398ba7e
gnehs <jayuiop@gmail.com>
2023-11-20 17:16:50 +0800

    added @tailwindcss/typography

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,7 +2,10 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1> <Outlet />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="prose">
+        <Outlet />
+      </div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
