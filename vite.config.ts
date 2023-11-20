9113cf6492a8018150f2923416ac324ab81204e9
gnehs <jayuiop@gmail.com>
2023-11-20 18:07:06 +0800

    fixed gtm w/ prerender

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,19 @@ export default defineConfig({
     react(),
     seoPrerender({
       routes: globSync("./src/content/**/*.mdx").map(parsePath),
+      callback(html, route) {
+        const gtm = [
+          `<!-- Google Tag Manager -->`,
+          `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':`,
+          `new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],`,
+          `j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=`,
+          `'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);`,
+          `})(window,document,'script','dataLayer','GTM-NPVBCDZ');</script>`,
+          `<!-- End Google Tag Manager -->`,
+        ].join("\n");
+
+        return html.replace("</body>", `${gtm}</body>`);
+      },
     }),
   ],
 });
